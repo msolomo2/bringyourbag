1e2c87c56645219c001aa28d63277290d8d9faf6
Michael Lobel <michael@mytennislessons.com>
2013-09-29 13:32:23 -0500

    sketch of twilio design

--- bringyourbag.rb
+++ bringyourbag.rb
@@ -19,3 +19,21 @@ end
 get '/events' do
 	haml :events
 end
+
+get '/call_alderman/:zipcode' do
+	Alderman.call_alderman(params[:zipcode])
+end
+
+class Alderman do
+
+	def self.call_alderman(zipcode) do
+		alderman = find_alderman(zipcode)
+		TwilioApi.make_call(alderman.number)
+	end
+
+	def find_alderman(zipcode) do
+		alderman = Aldermen.find_by(:zipcode, zipcode)
+		TwilioApi.choose_alderman(alderman) if alderman.count > 1
+	end
+
+end
