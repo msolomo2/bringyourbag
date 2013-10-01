e356dd9799214ca91e059c587e0b962e5df1e297
Michael Lobel <michael@mytennislessons.com>
2013-09-30 19:54:22 -0500

    forgot to comment out an end

--- bringyourbag.rb
+++ bringyourbag.rb
@@ -20,20 +20,25 @@ get '/events' do
 	haml :events
 end
 
-get '/call_alderman/:zipcode' do
-	Alderman.call_alderman(params[:zipcode])
-end
+# get '/call_alderman/:zipcode' do
+# 	Alderman.call_alderman(params[:zipcode])
+# end
 
-class Alderman do
+# class Alderman do
 
-	def self.call_alderman(zipcode) do
-		alderman = find_alderman(zipcode)
-		TwilioApi.make_call(alderman.number)
-	end
+# 	def self.call_alderman(zipcode) do
+# 		alderman = find_alderman(zipcode)
+# 		TwilioApi.make_call(alderman.number)
+# 		# We have to make sure to store the users number,
+# 		# so that they can be sent follow up text messages
+# 		# when it's times to call their alderman again
+# 		#Also, as an open source project this should have some very
+# 		#clear and visible privacy 
+# 	end
 
-	def find_alderman(zipcode) do
-		alderman = Aldermen.find_by(:zipcode, zipcode)
-		TwilioApi.choose_alderman(alderman) if alderman.count > 1
-	end
+# 	def find_alderman(zipcode) do
+# 		alderman = Aldermen.find_by(:zipcode, zipcode)
+# 		TwilioApi.choose_alderman(alderman) if alderman.count > 1
+# 	end
 
-end
+# end
